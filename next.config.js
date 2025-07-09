d1673908172cbdcd13746f69efa8aacd6cdd7f56
Wulan Lailatul Mufidah <wlnlailatul@gmail.com>
2025-07-09 13:07:36 +0700

    second commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-const { i18n } = require('./next-i18next.config');
+// const { i18n } = require('./next-i18next.config');
 
-module.exports = {
-  i18n,
+const nextConfig = {
+  // i18n,
   reactStrictMode: true,
+  output: 'export', // penting untuk Netlify
+  images: {
+    unoptimized: true, // wajib agar <Image /> bisa dipakai di Netlify
+  },
 };
+
+module.exports = nextConfig;
